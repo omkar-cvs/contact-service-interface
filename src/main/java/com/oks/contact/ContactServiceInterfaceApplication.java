d78d1fcd2c76c5d59f5c8af31135c78389327b68
Omkar Singh <omkarsinghaws@gmail.com>
2024-08-28 21:21:57 +0530

    first commit

---
+++ src/main/java/com/oks/contact/ContactServiceInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.oks.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactServiceInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactServiceInterfaceApplication.class, args);
+	}
+
+}
